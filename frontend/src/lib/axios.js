981eea991234ea5693e04060e686091c7bad9114
Ayush Srivastava <ayush261102@hotmail.com>
2026-01-11 00:16:49 +0530

    Add Axios debug interceptor

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -5,4 +5,10 @@ const axiosInstance = axios.create({
   withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
 });
 
+axiosInstance.interceptors.request.use((config) => {
+  const fullUrl = config.baseURL + config.url;
+  console.log(`🔍 [AXIOS] Request: ${config.method.toUpperCase()} ${fullUrl}`);
+  return config;
+});
+
 export default axiosInstance;
